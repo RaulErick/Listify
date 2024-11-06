1046d94e60541554ebbfaad584c5a96d38a03e3a
Raul <raulerick@sempreceub.com>
2024-11-06 17:53:18 -0300

    v1 das notas

--- app.py
+++ app.py
@@ -4,28 +4,23 @@ from db_connection import create_connection, close_connection
 
 app = Flask(__name__)
 
-
 # Rota para a página inicial (landing page)
 @app.route("/", methods=["GET"])
 def landing_page():
     return render_template("landing_page.html")
-
 
 # Rota para a página de login
 @app.route("/login", methods=["GET"])
 def login():
     return render_template("login.html")
 
-
-@app.route("/login/criar_conta", methods=["GET"])
+@app.route("/criar_conta", methods=["GET"])
 def criar_conta():
     return render_template("criar_conta.html")
 
-
-@app.route("/login/recuperar_conta", methods=["GET"])
+@app.route("/recuperar_conta", methods=["GET"])
 def recuperar_conta():
     return render_template("recuperar_conta.html")
-
 
 @app.route("/home", methods=["GET", "POST"])
 def home():
@@ -34,11 +29,8 @@ def home():
         if id_lista:  # Verifica se o campo não está vazio
             return redirect(url_for("lista", id_lista=id_lista))
         else:
-            return render_template(
-                "home.html", error="Por favor, insira um ID de lista válido."
-            )
+            return render_template("home.html", error="Por favor, insira um ID de lista válido.")
     return render_template("home.html")
-
 
 # Função para gerar um ID de lista único (6 dígitos)
 def gerar_id_unico():
@@ -61,7 +53,6 @@ def gerar_id_unico():
 
     return id_lista_aleatorio
 
-
 # Rota para criar uma nova lista
 @app.route("/criar_lista", methods=["POST"])
 def criar_lista():
@@ -82,7 +73,6 @@ def criar_lista():
 
     return redirect(url_for("lista", id_lista=id_lista_aleatorio))
 
-
 # Rota para exibir a lista
 @app.route("/lista/<int:id_lista>", methods=["GET"])
 def lista(id_lista):
@@ -102,7 +92,6 @@ def lista(id_lista):
     else:
         return render_template("not_found.html")
 
-
 @app.route("/atualizar_nome_lista/<int:id_lista>", methods=["POST"])
 def atualizar_nome_lista(id_lista):
     novo_nome = request.form.get("novo_nome_lista")
@@ -120,42 +109,47 @@ def atualizar_nome_lista(id_lista):
 
     return "Nome atualizado com sucesso", 200
 
-
-# CRUD para itens
-@app.route("/lista/<int:lista_id>/item", methods=["POST"])
-def adicionar_item(lista_id):
-    nome_item = request.form.get("nome_item")
-    anotacoes_item = request.form.get("anotacoes_item", "")
+# Rota para atualizar uma nota específica de um item
+@app.route("/lista/<int:lista_id>/item/<int:item_id>", methods=["PUT"])
+def atualizar_nota(lista_id, item_id):
+    anotacoes_item = request.form.get("anotacoes_item")
 
     conn = create_connection()
     cursor = conn.cursor()
-
-    # Inserir o item na tabela Itens
-    cursor.execute(
-        "INSERT INTO Itens (Nome_Item, Anotacoes_item) VALUES (%s, %s)",
-        (nome_item, anotacoes_item),
-    )
-    item_id = cursor.lastrowid
-
-    # Inserir na tabela de associação
-    cursor.execute(
-        "INSERT INTO Lista_has_Itens (Lista_idLista, Itens_idItens) VALUES (%s, %s)",
-        (lista_id, item_id),
-    )
+    cursor.execute("UPDATE Itens SET Anotacoes_item = %s WHERE idItens = %s", (anotacoes_item, item_id))
     conn.commit()
 
     cursor.close()
     close_connection(conn)
 
-    return jsonify({"status": "success", "item_id": item_id}), 201
+    return jsonify({"status": "success"}), 200
 
+@app.route("/lista/<int:lista_id>/item", methods=["POST"])
+def adicionar_item(lista_id):
+    nome_item = request.form.get("nome_item")
+    anotacoes_item = request.form.get("anotacoes_item", "")  # Anotação opcional
+    conn = create_connection()
+    cursor = conn.cursor()
+    
+    # Inserindo o novo item na tabela Itens
+    cursor.execute("INSERT INTO Itens (Nome_Item, Anotacoes_item) VALUES (%s, %s)", (nome_item, anotacoes_item))
+    item_id = cursor.lastrowid
+    
+    # Associando o item à lista
+    cursor.execute("INSERT INTO Lista_has_Itens (Lista_idLista, Itens_idItens) VALUES (%s, %s)", (lista_id, item_id))
+    conn.commit()
+    
+    cursor.close()
+    close_connection(conn)
+    
+    return jsonify({"status": "success", "item_id": item_id}), 201
 
 @app.route("/lista/<int:lista_id>/item", methods=["GET"])
 def obter_itens(lista_id):
     conn = create_connection()
     cursor = conn.cursor(dictionary=True)
-
-    # Obter todos os itens associados à lista
+    
+    # Consultando os itens e suas anotações associados a uma lista específica
     query = """
         SELECT i.idItens, i.Nome_Item, i.Anotacoes_item
         FROM Lista_has_Itens lhi
@@ -164,12 +158,11 @@ def obter_itens(lista_id):
     """
     cursor.execute(query, (lista_id,))
     itens = cursor.fetchall()
-
+    
     cursor.close()
     close_connection(conn)
-
+    
     return jsonify(itens), 200
-
 
 @app.route("/lista/<int:id_lista>", methods=["DELETE"])
 def excluir_lista(id_lista):
@@ -178,71 +171,27 @@ def excluir_lista(id_lista):
 
     # Excluir a lista da tabela Lista
     cursor.execute("DELETE FROM Lista WHERE idlista = %s", (id_lista,))
-
-    # Excluir todos os itens associados à lista
     cursor.execute("DELETE FROM Lista_has_Itens WHERE Lista_idLista = %s", (id_lista,))
-
-    conn.commit()
-    cursor.close()
-    close_connection(conn)
-
-    return (
-        jsonify(
-            {
-                "status": "success",
-                "message": f"Lista com ID {id_lista} excluída com sucesso!",
-            }
-        ),
-        200,
-    )
-
-
-@app.route("/lista/<int:lista_id>/item/<int:item_id>", methods=["PUT"])
-def atualizar_item(lista_id, item_id):
-    nome_item = request.form.get("nome_item")
-
-    conn = create_connection()
-    cursor = conn.cursor()
-
-    # Atualizar o item na tabela Itens
-    cursor.execute(
-        "UPDATE Itens SET Nome_Item = %s WHERE idItens = %s", (nome_item, item_id)
-    )
     conn.commit()
 
     cursor.close()
     close_connection(conn)
 
-    return jsonify({"status": "success"}), 200
-
+    return jsonify({"status": "success", "message": f"Lista com ID {id_lista} excluída com sucesso!"}), 200
 
 @app.route("/lista/<int:lista_id>/item/<int:item_id>", methods=["DELETE"])
 def excluir_item(lista_id, item_id):
     conn = create_connection()
     cursor = conn.cursor()
-
-    # Dissociar o item da lista
-    cursor.execute(
-        "DELETE FROM Lista_has_Itens WHERE Lista_idLista = %s AND Itens_idItens = %s",
-        (lista_id, item_id),
-    )
-
-    # Verificar se o item está associado a outras listas
-    cursor.execute(
-        "SELECT COUNT(*) FROM Lista_has_Itens WHERE Itens_idItens = %s", (item_id,)
-    )
+    cursor.execute("DELETE FROM Lista_has_Itens WHERE Lista_idLista = %s AND Itens_idItens = %s", (lista_id, item_id))
+    cursor.execute("SELECT COUNT(*) FROM Lista_has_Itens WHERE Itens_idItens = %s", (item_id,))
     count = cursor.fetchone()[0]
-
-    # Se o item não estiver associado a outras listas, excluí-lo
     if count == 0:
         cursor.execute("DELETE FROM Itens WHERE idItens = %s", (item_id,))
-
     conn.commit()
     cursor.close()
     close_connection(conn)
-
     return jsonify({"status": "success"}), 200
-
 
 if __name__ == "__main__":
     app.run(debug=True)
